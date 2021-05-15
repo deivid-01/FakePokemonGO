77bf8543e59854408cc21b0cb5fd9f7c7abd78d7
Deivid <datcx@hotmail.com>
2021-05-14 23:46:56 -0500

    Pokedex UI basic structure

--- Assets/Scripts/UI/UIPokedex.cs
+++ Assets/Scripts/UI/UIPokedex.cs
@@ -5,21 +5,32 @@ using UnityEngine.UI;
 public class UIPokedex : MonoBehaviour
 {
     public RawImage rawImage_picture;
+    
+    public InputField input_pkmName;
+    public List<RawImage> images_pkms;
+    public List<Button> btns_list;
+   
     public Text txt_name;
     public List<Text> txt_types;
 
+    TouchScreenKeyboard keyboard;
+
     private void OnEnable()
     {
-        GameEvent.instance.OnPokedexFoundName += UpdateName;
-        GameEvent.instance.OnPokedexFoundTypes += UpdateTypes;
-        GameEvent.instance.OnPokedexFoundImage+= UpdateImage;
+     //   GameEvent.instance.OnPokedexFoundName += UpdateName;
+     //   GameEvent.instance.OnPokedexFoundTypes += UpdateTypes;
+       // GameEvent.instance.OnPokedexFoundImage+= UpdateImage;
     }
 
     private void OnDisable()
     {
-        GameEvent.instance.OnPokedexFoundName -= UpdateName;
-        GameEvent.instance.OnPokedexFoundTypes -= UpdateTypes;
-        GameEvent.instance.OnPokedexFoundImage -= UpdateImage;
+      //  GameEvent.instance.OnPokedexFoundName -= UpdateName;
+      //  GameEvent.instance.OnPokedexFoundTypes -= UpdateTypes;
+       // GameEvent.instance.OnPokedexFoundImage -= UpdateImage;
+    }
+    private void Start()
+    {
+        SetTouchKeyboard();
     }
 
     void UpdateName(string name)
@@ -47,8 +58,61 @@ public class UIPokedex : MonoBehaviour
         
     }
 
-    public void SearchPokemon(string name)
+    public void SearchPokemon(InputField inputTxt)
     {
-        GameEvent.instance.PokemonCaptured(name);
+        
+       // GameEvent.instance.PokemonCaptured(input_pkmName.text);
+        print("Searching pokemon with name " + inputTxt.text);
     }
+    public void SearchPokemon(Text txt_name)
+    {
+        string pkm_name = txt_name.text;
+        //GameEvent.instance.SearchPokemon(pkm_name)
+        print("Searching pokemon with name " + pkm_name);
+    }
+
+    public void NextPage() 
+    {
+        //GameEvent.instance.LoadNextList()
+        print("Loading next page");
+    }
+    public void PreviousPage()
+    {
+        //  GameEvent.instance.LoadPreviousList();
+        print("Loading previous Page");
+    }
+
+    void SetTouchKeyboard()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, autocorrection: true);
+        keyboard.active = true;
+    }
+    public void OpenTouchKeyBoard()
+    {
+        print("Trying to open keyboard");
+
+        if (keyboard != null)
+        {
+            if (!keyboard.active)
+            {
+                keyboard.active = true;
+        print(keyboard.active);
+            }
+        }
+
+
+    }
+    public void CloseTouchKeyBoard()
+    {
+        print("Trying to close keyboard");
+
+        if (keyboard != null)
+        {
+            if (keyboard.active)
+            {
+                keyboard.active = false;
+            }
+        }
+    }
+
 }
